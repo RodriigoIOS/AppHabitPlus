94b7f040c6d7e90a5a0192ee14ba2eadde7faacd
RodriigoiOS <Rodriigo.dev@outlook.com>
2025-12-29 00:35:10 -0300

    Creating project

---
+++ AppHabitPlus/AppHabitPlus/AppHabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppHabitPlusApp.swift
+//  AppHabitPlus
+//
+//  Created by Rodrigo on 29/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AppHabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
